b365bab31f9fc25cce64cb07665e7c9e0fc28077
Geanmarco Lima dos Reis <geanmarco.reis03@gmail.com>
2023-09-27 20:25:05 -0300

    Projeto Finalizado

---
+++ index.js
@@ -0,0 +1,12 @@
+var imagemprincipal = window.document.getElementById("imagemprincipal") 
+var imagemsecundaria = window.document.getElementById("imagemsecundaria")
+
+function AlterarImagem(){
+    imagemprincipal.style = "display:none"
+    imagemsecundaria.style = "display:flex"
+}
+
+function AlterarImagem2(){
+    imagemsecundaria.style = "display:none"
+    imagemprincipal.style = "display:flex"
+}
